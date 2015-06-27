51d1df469166199bb81c308a64f4795eb54d3b6f
Sharif <s.paksa@icloud.com>
2015-06-27 03:34:36 -0700

    No extra request when there are no submissions that can be loaded.

--- resources/views/scripts/load-more-submissions.blade.php
+++ resources/views/scripts/load-more-submissions.blade.php
@@ -1,26 +1,36 @@
 <script>
+	var canLoadMore = true;
+
 	$('#loadMoreSubmissions').click(function()
 	{
 		var button = $(this);
 
-		button.removeClass('error').addClass('loading');
-		button.html('<img src="{{ url("/img/dark-loader.svg") }}" width="25">');
-
-		$.get('{{ url("/more/front") }}')
-		.done(function(data)
+		if (canLoadMore)
 		{
-			button.removeClass('loading');
-			button.html('Load more submissions.');
+			button.removeClass('error').addClass('loading');
+			button.html('<img src="{{ url("/img/dark-loader.svg") }}" width="25">');
 
-			if (data == '')
-				button.html('No more submissions left.');
-			else
-				$('.questions-list').append(data);
-		})
-		.fail(function(res)
-		{
-			button.removeClass('loading').addClass('error');
-			button.text(res.responseText);
-		});
+			$.get('{{ url("/more/front") }}')
+			.done(function(data)
+			{
+				button.removeClass('loading');
+				button.html('Load more submissions.');
+
+				if (data == '')
+				{
+					button.html('No more submissions left.');
+					canLoadMore = false;
+				}
+				else
+				{
+					$('.questions-list').append(data);
+				}
+			})
+			.fail(function(res)
+			{
+				button.removeClass('loading').addClass('error');
+				button.text(res.responseText);
+			});
+		}
 	});
 </script>
