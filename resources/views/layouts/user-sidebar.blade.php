7f77aa7d4a9498423781f442b5297d9e629b326c
Sharif <s.paksa@icloud.com>
2015-06-27 02:33:14 -0700

    User sidebar changes.

--- resources/views/layouts/user-sidebar.blade.php
+++ resources/views/layouts/user-sidebar.blade.php
@@ -26,15 +26,4 @@
 		<li>-</li>
 		@endif
 	</ul>
-	<hr>
-	<h6 class="super-header">Sponsored</h6>
-	<ul class="no-bullet">
-		<li><a href=""><i class="ion-help"></i> Sponsored question?</a></li>
-		<li><a href=""><i class="ion-pound"></i> SponsoredTag</a></li>
-		<li><a href=""><i class="ion-link"></i> Unsplash - Royalty free HD images</a></li>
-		<li><a href=""><i class="ion-play"></i> Advertisment video</a></li>
-		<li><a href=""><i class="ion-headphone"></i> Link to a song</a></li>
-		<li><a href=""><i class="ion-image"></i> Image</a></li>
-		<li><a href=""><i class="ion-person-add"></i> Jobs</a></li>
-	</ul>
 </div>
