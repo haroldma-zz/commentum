d94a326ef58492e843f45da5e837609904fc20f0
Sharif <s.paksa@icloud.com>
2015-07-02 22:07:53 -0700

    Maintenance page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -3,9 +3,20 @@
 @section('page')
 <div class="error-page">
     <div class="content">
-        <h1>
-            Be right back.
-        </h1>
+        <div class="row">
+        	<div class="medium-6 medium-offset-3 columns text-center">
+        		<img src="https://i.imgur.com/MvlADL6.jpg" width="200px">
+        		<br>
+        		<br>
+        		<hr>
+        		<h1><b>BRB</b></h1>
+        		<h5>
+        			Due to unexpected increase of users, we are moving to another server.
+        			<br><br>
+        			We'll be back in a few minutes.
+        		</h5>
+        	</div>
+        </div>
     </div>
 </div>
 @stop
