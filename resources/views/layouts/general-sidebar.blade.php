0a2db4b2a14d576e1b331e1dd844b5ce56eb3897
Ethan Barron <ebarron@uco.edu>
2015-07-04 14:15:24 -0500

    updated view to use Tag model's getTrendingTags() method

--- resources/views/layouts/general-sidebar.blade.php
+++ resources/views/layouts/general-sidebar.blade.php
@@ -1,8 +1,8 @@
 <div class="panel small sidebar">
-	<h6 class="super-header">New tags</h6>
+	<h6 class="super-header">Trending tags</h6>
 	<ul class="no-bullet">
-		@foreach(App\Models\Tag::getNewTags() as $tag)
-		<li><a href="{{ url($tag->permalink()) }}">#{{ $tag->display_title }}</a></li>
+		@foreach(App\Models\Tag::getTrendingTags() as $tag)
+			<li><a href="{{ url($tag->permalink()) }}">#{{ $tag->display_title }}</a></li>
 		@endforeach
 	</ul>
 	<hr>
