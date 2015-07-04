c900fce2135a4d17d63714f7515eb1d2ba55380a
zumicts <harry@zumicts.com>
2015-07-03 22:10:27 -0400

    Perfecting balance between new and popular content (hot)

---
+++ database/migrations/2015_07_04_020828_no_zero_momentum.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class NoZeroMomentum extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $sql = <<<SQL
+DROP FUNCTION IF EXISTS calculateThreadMomentum;
+CREATE FUNCTION calculateThreadMomentum(impressions INTEGER, views INTEGER, comment_momentum DOUBLE)
+RETURNS DOUBLE
+BEGIN
+    DECLARE momentum DOUBLE;
+    SET momentum = (views * (views / impressions) + comment_momentum) * 10;
+
+    IF (momentum > 0) then
+    	return momentum;
+    END IF;
+    return 1;
+END
+SQL;
+        DB::unprepared($sql);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $sql = <<<SQL
+DROP FUNCTION IF EXISTS calculateThreadMomentum;
+CREATE FUNCTION calculateThreadMomentum(impressions INTEGER, views INTEGER, comment_momentum DOUBLE)
+RETURNS DOUBLE
+BEGIN
+    RETURN (views * (views / impressions) + comment_momentum) * 10;
+END
+SQL;
+        DB::unprepared($sql);
+    }
+}
