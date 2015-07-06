49c66ba3e4204c8b0e95f49b317ad418fef6d987
zumicts <harry@zumicts.com>
2015-07-06 13:34:58 -0400

    Add mobile support

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	<title>{{ (Auth::check() && Auth::user()->messageCount() > 0 ? "[" . Auth::user()->messageCount() . "] " : "") }}@yield('title', 'Commentum')</title>
 	{!! HTML::style('/stylesheets/app.css') !!}
 </head>
