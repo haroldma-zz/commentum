7f25b9f39d8f54e16b4098307b27f8e233fa4e10
Ethan Barron <ebarron@uco.edu>
2015-07-06 10:38:34 -0500

    configured embed-js settings

--- resources/views/scripts/embed-js.blade.php
+++ resources/views/scripts/embed-js.blade.php
@@ -1,13 +1,13 @@
 <script>
-	$('#content-embeddable').embedJS({
+	$('#content-embeddable').embedBlock({
       //The selector(id/class/tagName) inside #element that needs to be processed
-      embedSelector   :'div',
+      //embedSelector   :'div',
       //Instructs the library whether or not to embed urls
-      link              : true,
+      //link              : true,
       //same as the target attribute in html anchor tag . supports all html supported target values.
-      linkTarget        : '_blank',
+      //linkTarget        : '_blank',
       //Array of extensions to be excluded from converting into links
-      linkExclude       : ['jpg','pdf'],
+      //linkExclude       : ['jpg','pdf'],
       //set false to show a preview of document(pdf,xls,xlsx,doc,docx,ppt) links
       docEmbed          : true,
       docOptions        : {
@@ -30,7 +30,7 @@
       videoHeight       : 390,
       //( Mandatory ) The authorization key obtained from google's developer console for
       //using youtube data api and map embed api
-      gdevAuthKey         : 'xxxxxxx',
+      gdevAuthKey         : 'AIzaSyD-1RoxCSkWat6q9FZfLykk2lDBrIz5lVY',
       //Set google map location embed
       // Use @(place-name) to use this feature . Eg: @(Sydney)
       locationEmbed       :true,
@@ -39,8 +39,9 @@
             mode: 'place'                   
       },
       //Instructs the library whether or not to highlight code syntax.
-      highlightCode     : true,
+      //highlightCode     : true,
       //Instructs the library whether or not embed the tweets
+      /*
       tweetsEmbed     : true,
       tweetOptions:{
             //The maximum width of a rendered Tweet in whole pixels. Must be between 220 and 550 inclusive.
@@ -58,13 +59,14 @@
             //Supported Languages listed here (https://dev.twitter.com/web/overview/languages)
             lang       : 'en'
       },
+      */
       //An array of services excluded from embedding...
       //Options : codePen/jdFiddle/jsBin/ideone/plunker/soundcloud/twitchTv/dotSub/dailymotion/vine/ted/liveleak/spotify/ustream
       //          /flickr/instagram
       //Can exclude all options by setting it to 'all'
-      excludeEmbed     :['twitchTv'],
+      //excludeEmbed     :['twitchTv'],
       //Height of jsfiddle/codepen/jsbin/ideone/plunker
-      codeEmbedHeight  :300,
+      codeEmbedHeight : 300,
       soundCloudOptions: {
           height      : 160,
           themeColor  : 'f50000',    //Hex Code of the player theme color
@@ -75,26 +77,26 @@
           showReposts : false,
           visual      : false,       //Show/hide the big preview image
           download    : false        //Show/Hide download buttons
-              },
+      },
       vineOptions:{
             maxWidth:null,
             type:'postcard',         //'postcard' or 'simple' embedding
             responsive:false         // whether to make the vine embed responsive
-      },
+      }//,
       //callback before doc preview
-      beforeDocPreview  : function(){},
+      //beforeDocPreview  : function(){},
       //callback after doc preview
-      afterDocPreview   : function(){},
+      //afterDocPreview   : function(){},
       // callback on video frame view
-      onVideoShow:function(){},
+      //onVideoShow:function(){},
       //callback on video load (youtube/vimeo)
-      onVideoLoad:function(){}
+      //onVideoLoad:function(){}
       //function to execute before embedding services
-      beforeEmbedJSApply: function () {},
+      //beforeEmbedJSApply: function () {},
       //callback after embedJS is applied
-      afterEmbedJSLApply: function () {},
+      //afterEmbedJSLApply: function () {},
       //callback after the twitter widgets of a BLOCK are loaded.
-      onTwitterShow     : function () {}
+      //onTwitterShow     : function () {}
 
 	});
 </script>
