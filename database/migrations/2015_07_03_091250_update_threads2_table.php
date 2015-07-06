dd50a64367a00b2eca3d73f7582e83c123a30b0f
Sharif <s.paksa@icloud.com>
2015-07-06 13:17:49 +0200

    Had to rename filename as well as Class name to prevent conflicts while migrating

--- database/migrations/2015_07_03_091250_update_threads2_table.php
+++ database/migrations/2015_07_03_091250_update_threads2_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Update2ThreadsTable extends Migration
+class UpdateThreads2Table extends Migration
 {
     /**
      * Run the migrations.
