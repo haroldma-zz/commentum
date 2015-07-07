b34824a5bfc934fc3761e6fea1585fc20d65042f
Ethan Barron <ebarron@uco.edu>
2015-07-07 12:29:44 -0500

    added rules/policy

---
+++ resources/views/pages/policy.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.default')
+
+@section('page')
+<div class="hero">
+	<div class="row">
+		<div class="medium-4 medium-offset-4 columns">
+			<h3>Policy</h3>
+			
+			<hr>
+			
+			<p class="text-info">Running Commentum costs money -- a lot more than you'd think!</p>
+			<p class="text-info">We're hard at work, trying to make Commentum an even greater place to be.</p>
+			<p class="text-info">Help us pay our server costs by making a donation. Whatever amount you can help with is enough.</p>
+			<p class="text-info">We appreciate all the help we get. Thanks!</p>
+			
+			<p class="text-info">At Commentum, we respect and harbor freedom of speech. Freedom of speech means that you can share your opinion on anything. Freedom of speech, in our opinion, and by our policies, does not include:</p>
+			<ol class="numbered-list">
+				<li>Posting/sharing/viewing child pornography or "jailbait" (photos of young children in revealing attire posted/shared for sexual purposes).</li>
+				<li>"Doxxing" -- releasing personal information about another in an attempt to inteimidate, harrass, or otherwise terrorize that individual.</li>
+				<li>Rallying or calling to action support for a hate crime. While you are free to share your opinions on anything here on Commentum, we will not tolerate posts calling, literally, for the physical harm of a specific person or group of people based on race, religion, etc.</li>
+			</ol>
+			<p class="text-info">We have an absolute zero tolerance policy for any of the above types of content.</p>
+			
+		</div>
+	</div>
+</div>
+@stop
