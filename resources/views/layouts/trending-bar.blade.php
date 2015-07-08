10edcd01d1cede70af7cee6e0e5b998a4312d49a
Sharif <s.paksa@icloud.com>
2015-07-08 17:50:24 +0200

    Added a trending bar

---
+++ resources/views/layouts/trending-bar.blade.php
@@ -0,0 +1,28 @@
+<div class="row small-collapse trending-list">
+	<div class="medium-8 columns">
+		<ul class="inline-list">
+			<li><a href="{{ url('/t/all') }}">#all</a></li>
+			<li><i class="ion-ios-pulse-strong"></i></li>
+			<li><a href="{{ url('/t/random') }}">#random</a></li>
+			<li><a href="{{ url('/t/random') }}">#gonewild</a></li>
+			<li><a href="{{ url('/t/random') }}">#nsfw</a></li>
+			<li><a href="{{ url('/t/random') }}">#random</a></li>
+			<li><a href="{{ url('/t/random') }}">#gonewild</a></li>
+			<li><a href="{{ url('/t/random') }}">#nsfw</a></li>
+			<li><a href="{{ url('/t/random') }}">#random</a></li>
+			<li><a href="{{ url('/t/random') }}">#gonewild</a></li>
+			<li><a href="{{ url('/t/random') }}">#nsfw</a></li>
+			<li><a href="{{ url('/t/random') }}">#random</a></li>
+			<li><a href="{{ url('/t/random') }}">#gonewild</a></li>
+			<li><a href="{{ url('/t/random') }}">#nsfw</a></li>
+			<li><a href="{{ url('/t/random') }}">#random</a></li>
+			<li><a href="{{ url('/t/random') }}">#gonewild</a></li>
+			<li><a href="{{ url('/t/random') }}">#nsfw</a></li>
+		</ul>
+	</div>
+	<div class="medium-4 columns text-right">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, accusamus.
+		</p>
+	</div>
+</div>
