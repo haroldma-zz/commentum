2f9c7f0c5a9f2e84d631621578cd12448efde70b
Ethan Barron <ebarron@uco.edu>
2015-07-08 11:16:12 -0500

    renamed policy page to 'Rules & Policies' and fixed sidebar link

--- resources/views/layouts/general-sidebar.blade.php
+++ resources/views/layouts/general-sidebar.blade.php
@@ -21,7 +21,7 @@
 	</ul>
     <h6 class="super-header">Links</h6>
     <ul>
-        <li><a href="/p/policy">Privacy Policy</a></li>
+        <li><a href="/p/rules_and_policies">Rules & Policies</a></li>
         <li><a href="/p/donate">Donate</a></li>
         <li><a href="/p/contact">Contact</a></li>
     </ul>
