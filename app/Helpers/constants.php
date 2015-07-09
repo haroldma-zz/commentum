e79c4708cb9f7988cec85415047f91bbd44ac4db
zumicts <harry@zumicts.com>
2015-07-09 10:04:08 -0400

    Temp fix for regex

--- app/Helpers/constants.php
+++ app/Helpers/constants.php
@@ -13,10 +13,10 @@ class ROLES {
 class REGEXES {
     const API_EXTENSIONS = '^\\.\b(json|html|htm)\b';
 
-    const USERNAMES = "/(?:[^A-Za-z0-9]|^)(\/u\/\w{3,21})(?:(?!\w)|$)/gi";
+    const USERNAMES = "/(?:[^A-Za-z0-9[(]|^)(\/u\/\w{3,21})(?:(?!\w)|$)/gi";
     const USERNAMES_RP = " [$1]($1) ";
-    const TAGS = "/(?:[^A-Za-z0-9]|^)(?:\/t\/|#)(\w{2,35})(?:(?!\w)|$)/gi";
+    const TAGS = "/(?:[^A-Za-z0-9[(]|^)(?:\/t\/|#)(\w{2,35})(?:(?!\w)|$)/gi";
     const TAGS_RP = " [#$1](/t/$1) ";
-    const PAGES = "/(?:[^A-Za-z0-9]|^)(\/p\/\w{2,35})(?:(?!\w)|$)/gi";
+    const PAGES = "/(?:[^A-Za-z0-9[(]|^)(\/p\/\w{2,35})(?:(?!\w)|$)/gi";
     const PAGES_RP = " [$1]($1) ";
 }
